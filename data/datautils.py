a4c3ab2126cb91c0fc6fe6a364bb5c3f601a7094
levilain <benjamin.duguet@sfr.fr*>
2023-04-17 00:39:39 +0200

    little change

--- data/datautils.py
+++ data/datautils.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 def prepare_dataframe(originpath):
     imtrain = glob.glob(os.path.join(originpath, "*png"))
-    print(f"il y a {len(imtrain)} images ")
+    print(f"there are {len(imtrain)} images ")
 
     with open(os.path.join(originpath, "metadata.jsonl"), 'r') as jsonfile:
         json_list = list(jsonfile)
@@ -71,10 +71,7 @@ def yolo2dota(x, y, w, h, W, H):
 
 def keeplabel(rangepropose, label, tresh=0.5):
     """
-    On ne garde que les labels où x,y est dans l'image et si la hauteur et largeur de la bounding
-    box est au moins à 50% dans l'image. Pas de référence publi encore,
-
-    Point améliorable
+    We only keep labels where x,y are in the picture. If 50% of aera of bounding box is out of picture we do not label
 
     """
     x1, y1, x2, y2 = label
@@ -92,7 +89,7 @@ def keeplabel(rangepropose, label, tresh=0.5):
 
 def newlabel(rangepropose, label):
     """
-    il faut les labels en relatif par rapport à la tuile
+    we need label adapted for the tile
 
     """
     x1, y1, x2, y2 = label
@@ -104,111 +101,9 @@ def get_bbox(df, filename):
     return df[df['filename'] == filename].bbox.to_list()
 
 
-def tuile(filename, path, size, df, recover=0.5):
-    """
-    decouper les grosses image en tuile. Il boucler dedans, reflechir a un taux de recouvrement
-    verifier sur on conserve un label sur cette image, et il faut le recalculer
-
-    les perfs dépendront beaucoup de cette fonction je pense
-    """
-    img = cv2.imread(os.path.join(path, filename))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    Ylen, Xlen, channel = img.shape
-    list_tile = []
-    label_in = []
-    bbox = get_bbox(df, filename)
-    for nb_X in tqdm(range(0, Xlen, int(size * recover))):
-        for nb_Y in range(0, Ylen, int(size * recover)):
-            if nb_X + size < Xlen and nb_Y + size < Ylen:
-                label_tmp = []
-                list_tile.append(img[nb_Y:nb_Y + size, nb_X:nb_X + size, :])
-                for box in bbox:
-                    if keeplabel((nb_X, nb_X + size, nb_Y, nb_Y + size), box, tresh=0.5):
-                        label_tmp.append(newlabel((nb_X, nb_X + size, nb_Y, nb_Y + size), box))
-                label_in.append(label_tmp)
-    if not list_tile:
-        print(f"size of the img is to small: {img.shape}, take it all")
-        list_tile.append(img)
-        label_in.append(bbox)
-    return list_tile, label_in
-
-def tuile_convertYOLO_old(filename, path, dest, size, df, recover=0.5,RGB=True):
-    """
-    decouper les grosses image en tuile. Il boucler dedans, reflechir a un taux de recouvrement
-    verifier sur on conserve un label sur cette image, et il faut le recalculer
-
-    les perfs dépendront beaucoup de cette fonction je pense
-    """
-    img = cv2.imread(os.path.join(path, filename))
-    if RGB:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    destim = os.path.join(dest, 'images')
-    destlab = os.path.join(dest, 'labels')
-
-    Ylen, Xlen, channel = img.shape
-    list_tile = []
-    bbox = get_bbox(df, filename)
-    for nb_X in range(0, Xlen, int(size * recover)):
-        for nb_Y in range(0, Ylen, int(size * recover)):
-            if nb_X + size < Xlen and nb_Y + size < Ylen:
-                ima_tile = img[nb_Y:nb_Y + size, nb_X:nb_X + size, :]
-                name_imatile = filename.split('.')[0] + f"_{nb_X}_{nb_Y}"
-                list_tile.append(ima_tile)
-                for box in bbox:
-                    if keeplabel((nb_X, nb_X + size, nb_Y, nb_Y + size), box, tresh=0.25):
-                        adaptedbox = newlabel((nb_X, nb_X + size, nb_Y, nb_Y + size), box)
-                        x, y, w, h = dota2yolo(*adaptedbox, W=ima_tile.shape[1], H=ima_tile.shape[0])
-                        with open(os.path.join(destlab, name_imatile+".txt"), 'a') as f:
-                            f.write(f"{0} {x} {y} {w} {h}\n")
-
-                cv2.imwrite(os.path.join(destim, name_imatile+".png"), ima_tile)
-
-    if not list_tile:
-        print(f"size of the img {filename} is to small: {img.shape}")
-        if Xlen>size:
-            print(f"Xlen>size {Xlen>size} ")
-            for nb_X in range(0, Xlen, int(size * recover)):
-                ima_tile = img[:, nb_X:nb_X + size, :]
-                name_imatile = filename.split('.')[0] + f"_{nb_X}_{0}"
-                for box in bbox:
-                    if keeplabel((nb_X, nb_X + size, 0, Ylen), box, tresh=0.25):
-                        adaptedbox = newlabel((nb_X, nb_X + size, 0, Ylen), box)
-                        x, y, w, h = dota2yolo(*adaptedbox, W=ima_tile.shape[1], H=ima_tile.shape[0])
-                        with open(os.path.join(destlab, name_imatile+".txt"), 'a') as f:
-                            f.write(f"{0} {x} {y} {w} {h}\n")
-                cv2.imwrite(os.path.join(destim, name_imatile+".png"), ima_tile)
-
-        elif Ylen>size:
-            print(f"Ylen>size {Ylen>size} ")
-            for nb_Y in range(0, Ylen, int(size * recover)):
-                ima_tile = img[nb_Y:nb_Y + size,:, :]
-                name_imatile = filename.split('.')[0] + f"_{0}_{nb_Y}"
-                for box in bbox:
-                    if keeplabel((0, Xlen, nb_Y, nb_Y + size), box, tresh=0.25):
-                        adaptedbox = newlabel((0, Xlen, nb_Y, nb_Y + size), box)
-                        x, y, w, h = dota2yolo(*adaptedbox, W=ima_tile.shape[1], H=ima_tile.shape[0])
-                        with open(os.path.join(destlab, name_imatile+".txt"), 'a') as f:
-                            f.write(f"{0} {x} {y} {w} {h}\n")
-                cv2.imwrite(os.path.join(destim, name_imatile+".png"), ima_tile)
-
-        else:
-            print("neither one take it all")
-            cv2.imwrite(os.path.join(destim, filename), img)
-            print("bbox: ",bbox)
-            for box in bbox:
-                x, y, w, h = dota2yolo(*box, W=img.shape[1], H=img.shape[0])
-                with open(os.path.join(destlab, filename.split('.')[0] + ".txt"), 'a') as f:
-                    f.write(f"{0} {x} {y} {w} {h}\n")
-
-
 def tuile_convertYOLO(filename, path, dest, size, df, recover=0.5,RGB=True):
     """
-    decouper les grosses image en tuile. Il boucler dedans, reflechir a un taux de recouvrement
-    verifier sur on conserve un label sur cette image, et il faut le recalculer
-
-    les perfs dépendront beaucoup de cette fonction je pense
+     making tile from big picture
     """
     img = cv2.imread(os.path.join(path, filename))
     if RGB:
