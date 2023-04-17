f1902637044159348aa9a908fe6c351c039765c8
levilain <benjamin.duguet@sfr.fr*>
2023-04-17 08:17:05 +0200

    maj

--- data/datautils.py
+++ data/datautils.py
@@ -101,7 +101,7 @@ def get_bbox(df:pd.DataFrame, filename:str):
     return df[df['filename'] == filename].bbox.to_list()
 
 
-def tuile_convertYOLO(filename:str, path:str, dest:str, size:int, df:pd.DataFrame, recover:float=0.5,RGB=True):
+def tuile_convertYOLO_old(filename:str, path:str, dest:str, size:int, df:pd.DataFrame, recover:float=0.5,RGB=True):
     """
      making tile from big picture
     """
@@ -251,6 +251,72 @@ def tuile_convertYOLO(filename:str, path:str, dest:str, size:int, df:pd.DataFram
 
             cv2.imwrite(os.path.join(destim, name_imatile + ".png"), ima_tile)
 
+
+def tuile_convertYOLO(filename:str, path:str, dest:str, size:int, df:pd.DataFrame, recover:float=0.5,RGB=True):
+    """
+    we need label adapted for the tile
+    """
+    img = cv2.imread(os.path.join(path, filename))
+    if RGB:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    destim = os.path.join(dest, 'images')
+    destlab = os.path.join(dest, 'labels')
+
+    Ylen, Xlen, channel = img.shape
+    list_tile = []
+    bbox = get_bbox(df, filename)
+    for nb_X in range(0, Xlen, int(size * recover)):
+        for nb_Y in range(0, Ylen, int(size * recover)):
+            if nb_X + size < Xlen and nb_Y + size < Ylen:
+                ima_tile = img[nb_Y:nb_Y + size, nb_X:nb_X + size, :]
+                name_imatile = filename.split('.')[0] + f"_{nb_X}_{nb_Y}"
+                list_tile.append(ima_tile)
+                for box in bbox:
+                    if keeplabel((nb_X, nb_X + size, nb_Y, nb_Y + size), box, tresh=0.25):
+                        adaptedbox = newlabel((nb_X, nb_X + size, nb_Y, nb_Y + size), box)
+                        x, y, w, h = dota2yolo(*adaptedbox, W=ima_tile.shape[1], H=ima_tile.shape[0])
+                        with open(os.path.join(destlab, name_imatile+".txt"), 'a') as f:
+                            f.write(f"{0} {x} {y} {w} {h}\n")
+
+                cv2.imwrite(os.path.join(destim, name_imatile+".png"), ima_tile)
+
+    if not list_tile:
+        print(f"size of the img {filename} is to small: {img.shape}")
+        if Xlen>size:
+            print(f"Xlen>size {Xlen>size} ")
+            for nb_X in range(0, Xlen, int(size * recover)):
+                ima_tile = img[:, nb_X:nb_X + size, :]
+                name_imatile = filename.split('.')[0] + f"_{nb_X}_{0}"
+                for box in bbox:
+                    if keeplabel((nb_X, nb_X + size, 0, Ylen), box, tresh=0.25):
+                        adaptedbox = newlabel((nb_X, nb_X + size, 0, Ylen), box)
+                        x, y, w, h = dota2yolo(*adaptedbox, W=ima_tile.shape[1], H=ima_tile.shape[0])
+                        with open(os.path.join(destlab, name_imatile+".txt"), 'a') as f:
+                            f.write(f"{0} {x} {y} {w} {h}\n")
+                cv2.imwrite(os.path.join(destim, name_imatile+".png"), ima_tile)
+
+        elif Ylen>size:
+            print(f"Ylen>size {Ylen>size} ")
+            for nb_Y in range(0, Ylen, int(size * recover)):
+                ima_tile = img[nb_Y:nb_Y + size,:, :]
+                name_imatile = filename.split('.')[0] + f"_{0}_{nb_Y}"
+                for box in bbox:
+                    if keeplabel((0, Xlen, nb_Y, nb_Y + size), box, tresh=0.25):
+                        adaptedbox = newlabel((0, Xlen, nb_Y, nb_Y + size), box)
+                        x, y, w, h = dota2yolo(*adaptedbox, W=ima_tile.shape[1], H=ima_tile.shape[0])
+                        with open(os.path.join(destlab, name_imatile+".txt"), 'a') as f:
+                            f.write(f"{0} {x} {y} {w} {h}\n")
+                cv2.imwrite(os.path.join(destim, name_imatile+".png"), ima_tile)
+
+        else:
+            print("neither one take it all")
+            cv2.imwrite(os.path.join(destim, filename), img)
+            print("bbox: ",bbox)
+            for box in bbox:
+                x, y, w, h = dota2yolo(*box, W=img.shape[1], H=img.shape[0])
+                with open(os.path.join(destlab, filename.split('.')[0] + ".txt"), 'a') as f:
+                    f.write(f"{0} {x} {y} {w} {h}\n")
 
 
 class Shipclassifdataset(torch.utils.data.Dataset):
